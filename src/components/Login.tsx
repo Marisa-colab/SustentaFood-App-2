74a2860f70b0d30e95ef69ea4f6904724d428e32
Marisa-colab <zeliamarisa_1@hotmail.com>
2026-08-04 15:58:40 +0000

    Update import path for supabase client

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { supabase } from '../Lib/supabase';
+import { supabase } from '../supabaseClient';
 
  export default function Login() {
   const [email, setEmail] = useState('');
